cd494f9df144e209b478ce81aa1a0ec6dd31f9de
YiChia <ksph5219@gmail.com>
2016-07-11 20:52:25 +0800

    Update sample.php
    再取出陣列，計算陣列數
    跑for迴圈

--- sample.php
+++ sample.php
@@ -37,7 +37,7 @@ unset($xml);
 //----------------------存到二維陣列-------START-------------------------------//
 if(isset($tree["BookOrder"]["Order"]["ID"])) //判斷是否為單筆訂單
 {
-    
+    $orderID[0]=$tree["BookOrder"]["Order"]["ID"];
     if(isset($tree["BookOrder"]["Order"]["Detail"])) //訂購數目是否為單筆
     {
         $item[0][0]=$tree["BookOrder"]["Order"]["Detail"]["item"]["value"];
@@ -62,7 +62,8 @@ else
     $OrderNum=count($tree["BookOrder"]["Order"]); //計算訂單數
     for($i=0;$i<$OrderNum;$i++)
     {
-      
+       
+       $orderID[$i]=$tree["BookOrder"]["Order"][$i]["ID"];
       
         if(isset($tree["BookOrder"]["Order"][$i]["Detail"]["item"])) //客人訂購單一商品
         {
@@ -71,7 +72,7 @@ else
         }
         else
         {  //客人訂購多個商品
-          
+             $orderID[$i]=$tree["BookOrder"]["Order"][$i]["ID"];
             $Num=count($tree["BookOrder"]["Order"][$i]["Detail"])
       
             for($j=0;$j<Num;$j++)
@@ -87,10 +88,28 @@ else
 
 //方式A
 //原本的方式  //  但如果只有單筆 就會產生問題，目前也只想出這種寫法，不過好像很累贅
+/*
 $cntOrder=count($tree["BookOrder"]["Order"]);
 for($i=0;$i<$cntOrder;$i++)
 {
     $cntD=count($tree["BookOrder"]["Order"][$i]["Detail"]);
+    for($j=0;$j<$cntOrder;$j++)
+    {
+        $item[$i][$j];
+        $price[$i][$j];
+        echo "price".$price[$i][$j];
+        
+        //做後續的存資料庫處理
+        
+    }
+    
+}
+*/
+//目前解法
+$cntOrder=count($orderID);
+for($i=0;$i<$cntOrder;$i++)
+{
+    $cntD=count($item[$i]);
     for($j=0;$j<$cntOrder;$j++)
     {
         $item[$i][$j];
